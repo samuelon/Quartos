be3a0581eb452dda0c56cea23deff49ff5053786
Samuel On <samuelon3@gmail.com>
2019-11-22 15:35:40 -0500

    90% complete the lab9 w/ new creation of hwy and included new values

--- lab9/Timer.v
+++ lab9/Timer.v
@@ -1,21 +1,26 @@
-module Timer(Resetn,Clock,t_yellow,t_sec);
-output reg t_yellow, t_sec;
-input Resetn,Clock,State;
-output reg [29:0]Q;
+module Timer(Rstn,Clk, stChgd, t_yellow,t_sec);
 
-always @ (negedge Resetn, posedge Clock)
+output wire t_yellow, t_sec;
+input stChgd, Rstn,Clk;
+
+reg [29:0] Q;
+
+always @ (negedge Rstn, posedge Clk)
 begin
-	if (!Resetn )
-		Q <= 29'b0;
-//	else if (state == 1)
-//		Q <= 29'b0;
-	else if ( Q <= 29'H1FFFFFFF)
+	if (!Rstn )					//aclr
+		Q <= 30'b0;
+	else if (stChgd == 1'b1)
+		Q <= 30'b0;
+	else if ( Q < 30'h3FFFFFFF)
 		Q <= Q + 1'b1;
 	else
 		Q<=Q;
 end
 		//up counter for n amount of values
-assign t_yellow = Q[2];
-assign t_sec = Q[3];
+//assign t_yellow = Q[2];	for QSIM sims only
+//assign t_sec = Q[3];
+
+assign t_yellow = Q[28];
+assign t_sec = Q[29];
 
 endmodule
--- lab9/highway.v
+++ lab9/highway.v
@@ -1,26 +1,42 @@
-module highway(Resetn,Clock,S,P,Z);
-input Clock, Reset, w;
-output reg [2:0]z;
-//y is the present state
-//Y is the next state
+module highway(Resetn,Clock,CarPres,PedReq);
 
-input S,P
-reg[2:1]y,Y;
+input Clock, Resetn;
+input CarPres,PedReq;
+
+//currSt present state
+//nextSt next state
+reg[3:0]currSt,nextSt;
+
+reg pedStr, carStr; 
+
 reg [16:0]LEDR;
 reg [6:0]LEDG;
-assign z[0]
-wire t_sec, t_yellow;
- 
-parameter[3:0] hwy_G = 2'b0001, hwy_Y = 2'b0010, hwy_R = 2'b0011;
-parameter[3:0] sec_G = 2'b0100, sec_Y = 2'b0101, sec_R = 2'b0110;
-parameter[3:0] ped_G = 2'b0111, ped_Y = 2'b1000, ped_R = 2'b1001;
+
+reg carPednSrvd;  				// 1 car serviced. 0 ped is serviced. 
+
+wire stateChg; 
+
+wire tSec, tYel;
+
+parameter[3:0] hwy_G = 4'b0001, hwy_Y = 4'b0010, hwy_R = 4'b0011;
+parameter[3:0] sec_G = 4'b0100, sec_Y = 4'b0101, sec_R = 4'b0110;
+parameter[3:0] ped_G = 4'b0111, ped_Y = 4'b1000, ped_R = 4'b1001;
 //state for each road
 //Define the next state combinational circuit
 
+Timer my_timer (Resetn, Clock, stateChg, tYel, tSec);
+
+assign stateChg = currSt ^ nextSt; 
+
+initial begin
+carPednSrvd = 1'b0; 
+end
+
 always@(*)
 begin
-	case(y)
-	hwy_G:LEDG[6] = 1'b1; // green light hwy
+	case(currSt)
+	hwy_G:begin
+			LEDG[6] = 1'b1; // green light hwy
 			LEDG[4] = 1'b0;
 			LEDG[2] = 1'b0;
 			LEDR[16] = 1'b0;
@@ -28,12 +44,15 @@ begin
 			LEDR[12] = 1'b0;
 			LEDR[10] = 1'b1; // red light sec
 			LEDR[8] = 1'b0;
-			LEDR[6] = 1'b1; // red light ped
-		if(t_sec == 1'b1 || P == 1'b1 || S == 1'b1)
-				Y = hwy_Y;
+			LEDR[6] = 1'b1;	// red light ped
+			
+		if(tSec == 1'b1 && ( pedStr == 1'b1 || carStr == 1'b1)) 
+				nextSt = hwy_Y;
 		else //p_present || s_present == 1'b1);
-				Y = hwy_G;
-	hwy_Y:LEDG[6] = 1'b0;
+				nextSt = hwy_G;
+	end
+	hwy_Y:begin
+			LEDG[6] = 1'b0;
 			LEDG[4] = 1'b0;
 			LEDG[2] = 1'b0;
 			LEDR[16] = 1'b1; // yellow light hwy
@@ -42,32 +61,23 @@ begin
 			LEDR[10] = 1'b1; //redlight sec
 			LEDR[8] = 1'b0;
 			LEDR[6] = 1'b1; //red ligth ped
-		if (t_yellow == 1'b1)
-				Y = hwy_R;
-		else 
-				Y = hwy_Y;
-	hwy_R:LEDG[6] = 1'b0;
-			LEDG[4] = 1'b0;
-			LEDG[2] = 1'b0;
-			LEDR[16] = 1'b0;
-			LEDR[14] = 1'b1; // red light hwy
-			LEDR[12] = 1'b0;
-			LEDR[10] = 1'b1; //red light sec
-			LEDR[8] = 1'b0;
-			LEDR[6] = 1'b1; //red light ped
-		if ( S == 1'b0 && P == 1'b1)
-			begin
-			tflipflop check(S,Clock,Q);
-			if (Q)
-			Y = sec_G;
-			else
-			Y = ped_G;
-			end
-		else if (S)
-			Y = sec_G;
-		else if (P)
-			Y = ped_G;
-	sec_G:LEDG[6] = 1'b0;
+		if (tYel == 1'b1) begin
+				//nextSt = hwy_R;
+				if (pedStr == 1'b1 && carStr == 1'b0) begin
+					nextSt = ped_G;
+				end else if (pedStr == 1'b0 && carStr == 1'b1) begin
+					nextSt = sec_G; 
+				end 
+				// else if beging (pedStr == 1'b0 && carStr == 1'b1) begin
+				
+				//end
+				
+		end else begin 
+				nextSt = hwy_Y;
+		end
+	end 
+	sec_G:begin
+			LEDG[6] = 1'b0;
 			LEDG[4] = 1'b1; //Green light Sec_G
 			LEDG[2] = 1'b0;
 			LEDR[16] = 1'b0;
@@ -78,13 +88,15 @@ begin
 			LEDR[6] = 1'b1; //red light ped
 			//if there is an extra car, then prolong the time
 			//else per norm
-		if (C_detected == 1'b1 )
-			if (t_sec == 1'b1 )
-				Y = sec_Y;
-		else if ( t _yellow == 1)
-			Y = sec_Y;
-	
-	sec_Y:LEDG[6] = 1'b0;
+		if (carStr == 1'b1 )
+			if (tSec == 1'b1 )
+				nextSt = sec_Y;
+		else if ( tYel == 1)
+			nextSt = sec_Y;
+			end
+			
+	sec_Y:begin
+			LEDG[6] = 1'b0;
 			LEDG[4] = 1'b0;
 			LEDG[2] = 1'b0;
 			LEDR[16] = 1'b0;
@@ -93,11 +105,14 @@ begin
 			LEDR[10] = 1'b0;
 			LEDR[8] = 1'b0;
 			LEDR[6] = 1'b1; //redlight ped
-			if ( t_yellow)
-			Y = sec_R;
+			if ( tYel)
+			nextSt = sec_R;
 			else
-			Y = sec_Y;
-	sec_R:LEDG[6] = 1'b0;
+			nextSt = sec_Y;
+			end
+			
+	sec_R:begin
+			LEDG[6] = 1'b0;
 			LEDG[4] = 1'b0; 
 			LEDG[2] = 1'b0;
 			LEDR[16] = 1'b0;
@@ -108,7 +123,7 @@ begin
 			LEDR[6] = 1'b1; //red light ped
 			//create case for if the line up was ped next
 			//if ... go to ped_G else
-			Y = hwy_G;
+			nextSt = hwy_G;
 	ped_G:LEDG[6] = 1'b0;
 			LEDG[4] = 1'b0; 
 			LEDG[2] = 1'b1; // Green Light ped_G
@@ -118,11 +133,14 @@ begin
 			LEDR[10] = 1'b1; //red light sec
 			LEDR[8] = 1'b0;
 			LEDR[6] = 1'b0;
-			if (t_sec == 1) // green max time
-			Y = ped_Y;
+			if (tSec == 1) // green max time
+			nextSt = ped_Y;
 			else
-			Y = ped_G;
-	ped_Y:LEDG[6] = 1'b0;
+			nextSt = ped_G;
+			end
+			
+	ped_Y:begin
+			LEDG[6] = 1'b0;
 			LEDG[4] = 1'b0;
 			LEDG[2] = 1'b0;
 			LEDR[16] = 1'b0;
@@ -131,11 +149,13 @@ begin
 			LEDR[10] = 1'b1; //red light sec
 			LEDR[8] = 1'b1; // Yellow light ped_Y
 			LEDR[6] = 1'b0;
-			if ( t_yellow == 1)
-			Y = ped_R;
+			if ( tYel == 1)
+			nextSt = ped_R;
 			else
-			Y = ped_Y;
-	ped_R:LEDG[6] = 1'b0;
+			nextSt = ped_Y;
+			end
+	ped_R:begin
+			LEDG[6] = 1'b0;
 			LEDG[4] = 1'b0;
 			LEDG[2] = 1'b0;
 			LEDR[16] = 1'b0;
@@ -145,21 +165,29 @@ begin
 			LEDR[8] = 1'b0;
 			LEDR[6] = 1'b1; // Red light ped_R
 			//make case for if sec is pushed etc.
-			Y = hwy_G;
+			nextSt = hwy_G;
+			end
 	endcase
+end
 //Define the sequential block
 always@(negedge Resetn, posedge Clock)
-	if ( Resetn == 0)  y <=hwy_G;
-	else y <= Y;
-	
+begin
+	if ( Resetn == 0)  currSt <=hwy_G;
+	else currSt <= nextSt;
+end
 //Define pedestrian and Secondary operator
 always@(negedge Resetn, posedge Clock)
+begin
 	if (Resetn == 0)
 	begin
-		P <= 0;
-		C <= 0;
-	end
-	
+		pedStr <= 1'b0;
+		carStr <= 1'b0;
+	end else begin
+	 pedStr <= ~PedReq;
+	 carStr <= CarPres;
+	end 
+	 
+end
 	
 
 endmodule
